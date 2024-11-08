c56dbaef8eba9a966d62ea10fc110af5a462128a
unknown <keshavkokre30@gmail.com>
2024-11-08 09:34:54 +0530

    Conditional

--- linear.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main(){
-    int n=5;
-    for(int i =0;i<=n;i++){
-        printf("%d",i);
-    }
-}//linear time-O(n)
---
+++ nov 6/count.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int c=0;
+    while(1){
+        n=n/10;
+        c++;
+    }
+    return 0;
+
+}
